3b82daaf93a57963e81b568e828ea4a0085ffa35
crypto-assortments <32924756+crypto-assortments@users.noreply.github.com>
2017-11-05 22:20:44 +0800

    fix typo

--- src/version.h
+++ src/version.h
@@ -34,7 +34,7 @@ static const int PROTOCOL_VERSION = 60015;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 60014; // v1
-static const int MIN_PROTO_VERSION_FORK = 60016; // v1.1 up
+static const int MIN_PROTO_VERSION_FORK = 60015; // v1.1 up
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
